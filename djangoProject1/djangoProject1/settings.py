0112dba4ccdc8aeee62cc56f1e918ce2c21f89c1
Công Sơn <71420045+congsonzero@users.noreply.github.com>
2021-04-10 11:27:43 +0700

    update models and req

--- djangoProject1/djangoProject1/settings.py
+++ djangoProject1/djangoProject1/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'gymho.apps.GymhoConfig',
-    'corsheaders',
 ]
 
 MIDDLEWARE = [
--- djangoProject1/gymho/models.py
+++ djangoProject1/gymho/models.py
@@ -1,20 +1,19 @@
 from django.db import models
 
 
-
-# class Execise(models.Model):
-#     ExeciseID = models.IntegerField()
-#     name = models.CharField(max_length=25)
-#     rep_recommend = models.CharField(max_length=10)
-#     Active = models.BooleanField(default=True)
-
-
 class Exercise(models.Model):
-    exerciseid = models.IntegerField(db_column='ExerciseID', primary_key=True)  # Field name made lowercase.
-    name = models.CharField(max_length=25, blank=True, null=True)
-    rep_recommend = models.CharField(db_column='Rep recommend', max_length=10, blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters.
-    activate = models.BooleanField(db_column='Activate', blank=True, null=True)  # Field name made lowercase.
+    ExerciseID = models.IntegerField()
+    name = models.CharField(max_length=25)
+    rep_recommend = models.CharField(max_length=10)
+    Active = models.BooleanField(default=True)
 
-    class Meta:
-        # managed = False
-        db_table = 'Exercise'
+# class Exercise(models.Model):
+#     ExerciseID = models.IntegerField(db_column='ExerciseID', primary_key=True)  # Field name made lowercase.
+#     name = models.CharField(max_length=25, blank=True, null=True)
+#     Reprecommend = models.CharField(db_column='Reprecommend', max_length=10, blank=True, null=True)
+#     # Field name made lowercase. Field renamed to remove unsuitable characters.
+#     Activate = models.BooleanField(db_column='Activate', blank=True, null=True)  # Field name made lowercase.
+#
+#     class Meta:
+#         managed = False
+#         db_table = 'Exercise'
